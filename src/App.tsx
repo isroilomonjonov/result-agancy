fa9a2e304f5cb6a05941904af6c992a32b824064
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2024-05-09 21:51:07 +0500

    cities select added to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import messages_uz from './translations/uz.json';
 import { useContext } from 'react';
 import Main from './pages/main';
 import AppContext from './context/AppContext';
-const messages: { [key: string]: { greeting: string } } = {
+const messages: { [key: string]: { [id: string]: string } } = {
   ru: messages_ru,
   uz: messages_uz,
 };
@@ -15,15 +15,6 @@ function App() {
   return (
     <IntlProvider locale={ctx.language} messages={messages[ctx.language]}>
       <Main />
-      {/* <div className="App">
-        <h1>
-          <FormattedMessage id="greeting" />
-        </h1>
-        <select onChange={(e) => ctx.changeLanguage({ language: e.target.value })} value={ctx.language}>
-          <option value="uz">Uzbek</option>
-          <option value="ru">Russian</option>
-        </select>
-      </div> */}
     </IntlProvider>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {
     Select,
     SelectContent,
@@ -11,11 +11,70 @@ import {
 import AppContext, { AppContextType } from '@/context/AppContext';
 import { FormattedMessage } from 'react-intl';
 
+
+
 const Navbar = () => {
     const ctx: AppContextType = useContext(AppContext);
+    const [cities, setCities] = useState(ctx.language === "ru" ? [
+        {
+            value: "tashkent",
+            label: "Ташкент"
+        },
+        {
+            value: "tashkent-region",
+            label: "Ташкентская область"
+        }
+    ] : [
+        {
+            value: "tashkent",
+            label: "Tashkent"
+        },
+        {
+            value: "tashkent-region",
+            label: "Tashkent viloyati"
+        }
+    ])
+    useEffect(() => {
+        setCities(ctx.language === "ru" ? [
+            {
+                value: "tashkent",
+                label: "Ташкент"
+            },
+            {
+                value: "tashkent-region",
+                label: "Ташкентская область"
+            }
+        ] : [
+            {
+                value: "tashkent",
+                label: "Tashkent"
+            },
+            {
+                value: "tashkent-region",
+                label: "Tashkent viloyati"
+            }
+        ])
+    }, [ctx])
     return (
         <div>
-            <div className='navbar-top flex justify-between'>
+            <div className='container flex justify-between'>
+                <Select defaultValue='tashkent'>
+                    <SelectTrigger className="w-[180px] border-none focus:ring-transparent">
+                        <SelectValue placeholder="Select a City" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            {cities.map((city: { value: string, label: string }) => (
+                                <SelectItem id={city.value} value={city.value} key={city.value}>
+                                    <p className='text-[16px] font-medium flex'>
+                                        <img className='mr-2' width={"25px"} height={"18px"} src="/map-marker.svg" alt="location" />
+                                        {city.label}
+                                    </p>
+                                </SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
                 <Select onValueChange={(e) => ctx.changeLanguage({ language: e })} defaultValue='ru'>
                     <SelectTrigger className="w-[180px] border-none focus:ring-transparent">
                         <SelectValue placeholder="Select a Language" />
