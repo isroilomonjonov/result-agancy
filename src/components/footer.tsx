fa54bfb96298bdd808f5bb3a4df70e63d5704379
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2024-05-10 22:11:09 +0500

    hover effects added to footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,38 +11,38 @@ const Footer = () => {
                         <div className='flex flex-col gap-6'>
                             <p className='text-[#F8F8F8]'>Каталог услуг</p>
                             <ul className='flex flex-col gap-4'>
-                                <li><a href="#" className='text-[#0129E3] text-xl'>Медицинская консультация</a></li>
-                                <li><a href="#" className='text-xl'>Справочник лекарств</a></li>
-                                <li><a href="#" className='text-xl'>Справочник болезней</a></li>
-                                <li><a href="#" className='text-xl'>Справочник лекарств</a></li>
-                                <li><a href="#" className='text-xl'>Справочник Врачей и Клиник</a></li>
+                                <li><a href="#" className='text-[#0129E3] text-xl hover:underline'>Медицинская консультация</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Справочник лекарств</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Справочник болезней</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Справочник лекарств</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Справочник Врачей и Клиник</a></li>
                             </ul>
                         </div>
                         <div className='flex flex-col gap-6'>
                             <p className='text-[#F8F8F8]'>Выполнить</p>
                             <ul className='flex flex-col gap-4'>
-                                <li><a href="#" className='text-xl'>Задать вопрос</a></li>
-                                <li><a href="#" className='text-xl'>Лучшие консультанты</a></li>
-                                <li><a href="#" className='text-xl'>Популярные вопросы</a></li>
-                                <li><a href="#" className='text-xl'>Регистрация</a></li>
-                                <li><a href="#" className='text-xl'>Вход в личный кабинет</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Задать вопрос</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Лучшие консультанты</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Популярные вопросы</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Регистрация</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Вход в личный кабинет</a></li>
                             </ul>
                         </div>
                         <div className='flex flex-col gap-6'>
                             <p className='text-[#F8F8F8]'>Проект</p>
                             <ul className='flex flex-col gap-4'>
-                                <li><a href="#" className='text-xl'>Информация о проекте</a></li>
-                                <li><a href="#" className='text-xl'>Сотрудничество</a></li>
-                                <li><a href="#" className='text-xl'>Контакты для связи</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Информация о проекте</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Сотрудничество</a></li>
+                                <li><a href="#" className='text-xl hover:underline'>Контакты для связи</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <p className='bg-white opacity-10 w-full h-[1px] mt-20'></p>
                 <div className='grid grid-cols-3 justify-items-center mt-7'>
-                    <p className='justify-self-start'><a href="#">MedYordam 2024 - Все права защищены</a></p>
-                    <p><a href="#">Обработка данных</a></p>
-                    <p className='justify-self-end'><a href="#">Условия использования</a></p>
+                    <p className='justify-self-start'><a href="#" className="hover:underline">MedYordam 2024 - Все права защищены</a></p>
+                    <p><a href="#" className="hover:underline">Обработка данных</a></p>
+                    <p className='justify-self-end'><a href="#" className="hover:underline">Условия использования</a></p>
                 </div>
             </div>
         </div>
